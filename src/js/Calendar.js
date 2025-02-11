6c894f4af104431b3340c4a85dcd2ba2d81526ac
Redvus <a.suvorof@gmail.com>
2025-02-11 16:30:09 +0400

    Сделал переключение по дням календаря и смену событий, которые сгенерировал.

--- src/js/Calendar.js
+++ src/js/Calendar.js
@@ -1,7 +1,11 @@
+import {gsap} from "gsap";
+import {floor, min} from "three/nodes";
+
 class Calendar {
     constructor() {
         this.initLayout();
-        // this.initEventsInside();
+        this.objectsTabs();
+        // this.initFilter();
     }
 
     initLayout() {
@@ -11,28 +15,100 @@ class Calendar {
 
         this.calendarMonth = document.createElement('ul');
         this.calendarMonth.className = 'calendar__month';
-
-        for (let i = 0; i < 31; i++) {
-            this.calendarDay = document.createElement('li');
-            this.calendarDay.className = 'calendar__month_date';
-            this.calendarDay.innerHTML = i + 1;
-            this.calendarMonth.appendChild(this.calendarDay);
-        }
+        this.calendarMonth.id = 'objectsTab';
 
         this.calendarEvents = document.createElement('ul');
         this.calendarEvents.className = 'calendar__events';
 
-        for (let i = 0; i < 10; i++) {
-            this.calendarEventsSingle = document.createElement('li');
-            this.calendarEventsSingle.className = 'calendar__events_single';
-            this.calendarEventsSingle.id = 'calendarEvent_' + `${i + 1}`;
+        // Event Random
+        let min = 2;
+        let max = 5;
+        this.calendarInsideRandom = Math.floor(Math.random() * (max - min + 1) + min);
 
-            this.calendarEventsSingle.innerHTML = `
-                <h4><span>1${i}:${i * 5}</span>&nbsp;&nbsp;<i class="fas fa-arrow-right"></i>&nbsp;&nbsp;Библиотека №${i + 1}</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum minima provident quas quisquam quos sit sunt temporibus veritatis voluptate. Culpa cumque deleniti dolorum facere facilis incidunt iste ratione voluptas?</p>
-            `;
+        // for (let i = 0; i < 31; i++) {
+        //     this.calendarDay = document.createElement('li');
+        //     this.calendarDay.className = 'calendar__month_date';
+        //     this.calendarDay.id = `calendarDate_${i}`;
+        //     this.calendarDay.innerHTML = `<span>${i + 1}</span>`;
+        //     // this.calendarDay.style.userSelect = 'none';
+        //     // this.calendarDay.style.pointerEvents = 'none';
+        //     this.calendarMonth.appendChild(this.calendarDay);
+        //
+        //     if (this.calendarDay.id === `calendarDate_${i}`) {
+        //         if (this.calendarDay.id === `calendarDate_${0}`) {
+        //             this.calendarDay.className = 'calendar__month_date is-visible';
+        //         }
+        //         this.calendarDay.setAttribute('data-id', `tab_${i + 1}`);
+        //         this.calendarEvent = document.createElement('div');
+        //         this.calendarEvent.id = `tab_${i + 1}`;
+        //         this.calendarEvent.className = 'calendar__events_inside';
+        //     }
+        //
+        //     for (let i = 0; i < this.calendarInsideRandom; i++) {
+        //         this.calendarEventSingle = document.createElement('li');
+        //         this.calendarEventSingle.className = 'calendar__events_single';
+        //         this.calendarEventSingle.id = 'calendarEvent_' + `${i + 1}`;
+        //
+        //         this.calendarEventSingle.innerHTML = `
+        //                 <h4><span>1${i}:${i * 5}</span>&nbsp;&nbsp;<i class="fas fa-arrow-right"></i>&nbsp;&nbsp;Библиотека №${i + 1}</h4>
+        //                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum minima provident quas quisquam quos sit sunt temporibus veritatis voluptate. Culpa cumque deleniti dolorum facere facilis incidunt iste ratione voluptas?</p>
+        //             `;
+        //
+        //         this.calendarEvent.appendChild(this.calendarEventSingle);
+        //         this.calendarEvents.appendChild(this.calendarEvent);
+        //     }
+        // }
 
-            this.calendarEvents.appendChild(this.calendarEventsSingle);
+        for (let i = 0; i < 31; i++) {
+            this.calendarDay = document.createElement('li');
+            this.calendarDay.className = 'calendar__month_date';
+            this.calendarDay.id = `calendarDate_${i}`;
+            this.calendarDay.innerHTML = `<span>${i + 1}</span>`;
+            // this.calendarDay.style.userSelect = 'none';
+            // this.calendarDay.style.pointerEvents = 'none';
+            this.calendarMonth.appendChild(this.calendarDay);
+
+            if (this.calendarDay.id === 'calendarDate_0') {
+                this.calendarDay.className = 'calendar__month_date is-visible';
+                this.calendarDay.setAttribute('data-id', 'tab_0');
+                this.calendarEvent = document.createElement('div');
+                this.calendarEvent.id = 'tab_0';
+                this.calendarEvent.className = 'calendar__events_inside';
+
+                for (let i = 0; i < 3; i++) {
+                    this.calendarEventSingle = document.createElement('li');
+                    this.calendarEventSingle.className = 'calendar__events_single';
+                    this.calendarEventSingle.id = 'calendarEvent_' + `${i + 1}`;
+
+                    this.calendarEventSingle.innerHTML = `
+                        <h4><span>1${i}:${(i + 5) * 5}</span>&nbsp;&nbsp;<i class="fas fa-arrow-right"></i>&nbsp;&nbsp;Библиотека №${i + 1}</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum minima provident quas quisquam quos sit sunt temporibus veritatis voluptate. Culpa cumque deleniti dolorum facere facilis incidunt iste ratione voluptas?</p>
+                    `;
+
+                    this.calendarEvent.appendChild(this.calendarEventSingle);
+                    this.calendarEvents.appendChild(this.calendarEvent);
+                }
+            } else if (this.calendarDay.id === 'calendarDate_1') {
+                this.calendarDay.className = 'calendar__month_date';
+                this.calendarDay.setAttribute('data-id', 'tab_1');
+                this.calendarEvent = document.createElement('div');
+                this.calendarEvent.id = 'tab_1';
+                this.calendarEvent.className = 'calendar__events_inside';
+
+                for (let i = 0; i < 5; i++) {
+                    this.calendarEventSingle = document.createElement('li');
+                    this.calendarEventSingle.className = 'calendar__events_single';
+                    this.calendarEventSingle.id = 'calendarEvent_' + `${i + 1}`;
+
+                    this.calendarEventSingle.innerHTML = `
+                        <h4><span>1${i}:${(i + 2) * 5}</span>&nbsp;&nbsp;<i class="fas fa-arrow-right"></i>&nbsp;&nbsp;Библиотека №2${i + 1}</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum minima provident quas quisquam quos sit sunt temporibus veritatis voluptate.?</p>
+                    `;
+
+                    this.calendarEvent.appendChild(this.calendarEventSingle);
+                    this.calendarEvents.appendChild(this.calendarEvent);
+                }
+            }
         }
 
         //Appends
@@ -41,12 +117,74 @@ class Calendar {
         this.calendar.appendChild(this.calendarEvents);
     }
 
-    initEventsInside() {
-        this.calendarEvent_1 = document.getElementById('calendarEvent_1');
-        this.calendarEvent_2 = document.getElementById('calendarEvent_2');
-        this.calendarEvent_3 = document.getElementById('calendarEvent_3');
-        this.calendarEvent_4 = document.getElementById('calendarEvent_4');
+    objectsTabs() {
+        this.tabMenuTrigger = document.querySelectorAll('.calendar__month_date');
+        this.arrayPanels = document.querySelectorAll('.calendar__events_inside');
+        let flag = 0;
+
+        for (let i = 0; i < this.tabMenuTrigger.length; i++) {
+            this.tabMenuTrigger[i].addEventListener('click', () => {
+                document.getElementsByClassName('is-visible')[0].classList.remove('is-visible');
+                this.tabMenuTrigger[i].classList.add('is-visible');
+                this.arrayTabs = Array.prototype.slice.call(this.tabMenuTrigger);
+                this.index = this.arrayTabs.indexOf(this);
+
+                this.arrayPanels.forEach(tab => {
+                    gsap.set(tab, {
+                        autoAlpha: 0,
+                        display: 'none'
+                    });
+                });
+
+                let tl = gsap.timeline();
+                tl
+                    .fromTo(this.arrayPanels[i], {
+                        autoAlpha: 0
+                    }, {
+                        duration: 0.3,
+                        autoAlpha: 1,
+                        display: 'flex',
+                        ease: 'power2.in'
+                    })
+                ;
+            }, false);
+
+            if (this.tabMenuTrigger[i] !== this.tabMenuTrigger[0]) {
+                gsap.set(this.arrayPanels, {
+                    autoAlpha: 0,
+                    display: 'none'
+                });
+            }
+        }
+
+        if (flag === 0) {
+            gsap.set(this.arrayPanels[0], {
+                autoAlpha: 1,
+                display: 'flex'
+            });
+            flag = 1
+        }
+    }
+
+    initFilter() {
+        const
+            calendarEventsAll = document.querySelectorAll('.calendar__events_inside'),
+            calendarEvent = document.querySelectorAll('.calendar__events_single')
+        ;
+
+        calendarEventsAll.forEach((el) => {
+            calendarEvent.forEach(elem => {
+                if (!elem.textContent.includes('3')) {
+                    // for (let i = 0; i < this.mainContentCalendarDate.length; i++) {
+                    //     console.log(this.mainContentCalendarDate[i]);
+                    //     // this.mainContentCalendarDate[i].style.opacity = '0';
+                    // }
+                    console.log(elem.id);
+                    // el.style.display = 'none';
+                }
+            });
+        });
     }
 }
 
-export { Calendar }
+export {Calendar}
