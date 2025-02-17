c7d56dbdeb1dab67f467bdbcc78b17c69f0956b2
Redvus <a.suvorof@gmail.com>
2025-02-17 15:41:10 +0400

    Сверстал страницу к 80-летию победы.

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,14 @@ import '/scss/main.scss';
 import '/main.css';
 import { Anniversary2025 } from "./Anniversary2025";
 import {BannerTop} from "./BannerTop.js";
+import {Page80Event} from "./Page80Event.js";
+
 
 class Plugins {
     constructor() {
         // new Anniversary2025();
-        new BannerTop();
+        // new BannerTop();
+        new Page80Event();
     }
 }
 
