20e600e99c95ec289c5edb366a75763cf485940c
Redvus <a.suvorof@gmail.com>
2025-03-06 11:39:51 +0400

    Сделал анимацию и закачал на тестовый сервер.

--- src/js/Event_08_03_25.js
+++ src/js/Event_08_03_25.js
@@ -4,8 +4,8 @@ class Event_08_03_25 {
 
     constructor() {
         this.initLayout();
-        // this.initAnimation();
-        this.initDev();
+        this.initAnimation();
+        // this.initDev();
     }
 
     initLayout() {
@@ -20,6 +20,9 @@ class Event_08_03_25 {
         this.womanDay25.innerHTML = `
 			<picture id="womanDay25Back">
 				<img src="assets/images/womanDay25/womanDay25Back.svg" alt="">
+			</picture>
+			<picture id="womanDay25BackBottom">
+				<img src="assets/images/womanDay25/womanDay25Back_2.png" alt="">
 			</picture>
 		`;
 
@@ -108,96 +111,82 @@ class Event_08_03_25 {
     }
 
     initAnimation() {
-
         const
-            anniversary2025 = document.getElementById('anniversary2025'),
-            s2025_gift1_1 = document.getElementById('s2025_gift1_1'),
-            s2025_gift1_2 = document.getElementById('s2025_gift1_2'),
-            s2025_gift1_3 = document.getElementById('s2025_gift1_3'),
-            s2025_gift1_4 = document.getElementById('s2025_gift1_4'),
-            s2025_gift1_5 = document.getElementById('s2025_gift1_5'),
-            s2025_gift2_1 = document.getElementById('s2025_gift2_1'),
-            s2025_gift2_2 = document.getElementById('s2025_gift2_2'),
-            s2025_gift2_3 = document.getElementById('s2025_gift2_3'),
-            s2025_gift2_4 = document.getElementById('s2025_gift2_4'),
-            s2025_gift2_5 = document.getElementById('s2025_gift2_5'),
-            s2025_gift3_1 = document.getElementById('s2025_gift3_1'),
-            s2025_gift3_2 = document.getElementById('s2025_gift3_2'),
-            s2025_gift3_3 = document.getElementById('s2025_gift3_3'),
-            s2025_gift4_1 = document.getElementById('s2025_gift4_1'),
-            s2025_gift4_2 = document.getElementById('s2025_gift4_2'),
-            s2025_gift4_3 = document.getElementById('s2025_gift4_3'),
-            s2025_gift4_4 = document.getElementById('s2025_gift4_4'),
-            s2025_gift4_5 = document.getElementById('s2025_gift4_5'),
-            s2025_gift5_1 = document.getElementById('s2025_gift5_1'),
-            s2025_gift5_2 = document.getElementById('s2025_gift5_2'),
-            s2025_gift5_3 = document.getElementById('s2025_gift5_3'),
-            s2025_gift6_1 = document.getElementById('s2025_gift6_1'),
-            s2025_gift6_2 = document.getElementById('s2025_gift6_2'),
-            s2025_gift6_3 = document.getElementById('s2025_gift6_3'),
-            s2025_gift7_1 = document.getElementById('s2025_gift7_1'),
-            s2025_gift7_2 = document.getElementById('s2025_gift7_2'),
-            s2025_back = document.getElementById('s2025_back'),
-            s2025_dream = document.getElementById('s2025_dream'),
-            s2025_made = document.getElementById('s2025_made')
+            womanDay25 = document.getElementById('womanDay25'),
+            womanDay25Back = document.getElementById('womanDay25Back'),
+            womanDay25BackBottom = document.getElementById('womanDay25BackBottom'),
+            womanDay25Slogan_1 = document.getElementById('womanDay25Slogan_1'),
+            womanDay25Slogan_2 = document.getElementById('womanDay25Slogan_2'),
+            womanDay25Slogan_3 = document.getElementById('womanDay25Slogan_3'),
+            womanDay25Slogan_4 = document.getElementById('womanDay25Slogan_4'),
+            womanDay25Woman1_1 = document.getElementById('womanDay25Woman1_1'),
+            womanDay25Woman1_2 = document.getElementById('womanDay25Woman1_2'),
+            womanDay25Woman1_3 = document.getElementById('womanDay25Woman1_3'),
+            womanDay25Woman1_4 = document.getElementById('womanDay25Woman1_4'),
+            womanDay25Woman1_5 = document.getElementById('womanDay25Woman1_5'),
+            womanDay25Woman2_1 = document.getElementById('womanDay25Woman2_1'),
+            womanDay25Woman2_2 = document.getElementById('womanDay25Woman2_2'),
+            womanDay25Woman2_3 = document.getElementById('womanDay25Woman2_3'),
+            womanDay25Woman3_1 = document.getElementById('womanDay25Woman3_1'),
+            womanDay25Woman3_2 = document.getElementById('womanDay25Woman3_2'),
+            womanDay25Woman3_3 = document.getElementById('womanDay25Woman3_3'),
+            womanDay25Woman3_4 = document.getElementById('womanDay25Woman3_4'),
+            womanDay25Woman3_5 = document.getElementById('womanDay25Woman3_5'),
+            womanDay25Woman4_1 = document.getElementById('womanDay25Woman4_1'),
+            womanDay25Woman4_2 = document.getElementById('womanDay25Woman4_2'),
+            womanDay25Woman4_3 = document.getElementById('womanDay25Woman4_3'),
+            womanDay25Woman4_4 = document.getElementById('womanDay25Woman4_4'),
+            womanDay25Woman4_5 = document.getElementById('womanDay25Woman4_5')
         ;
 
         let tl = new gsap.timeline({
-            delay: 1,
+            delay: 0.5,
             onComplete: this.initHide(9)
         });
 
         tl
-            .to(anniversary2025, {
+            .to(womanDay25, {
                 duration: 0.3,
                 delay: '1',
                 autoAlpha: 1,
                 zIndex: 9999,
                 // easy: "elastic.in(1,0.3)"
             })
+            .from([womanDay25Back, womanDay25BackBottom], {
+                duration: 1,
+                delay: '0.1',
+                autoAlpha: 0
+            })
             .from([
-                s2025_gift1_1,
-                s2025_gift1_2,
-                s2025_gift1_3,
-                s2025_gift1_4,
-                s2025_gift1_5,
-                s2025_gift2_1,
-                s2025_gift2_2,
-                s2025_gift2_3,
-                s2025_gift2_4,
-                s2025_gift2_5,
-                s2025_gift3_1,
-                s2025_gift3_2,
-                s2025_gift3_3,
-                s2025_gift4_1,
-                s2025_gift4_2,
-                s2025_gift4_3,
-                s2025_gift4_4,
-                s2025_gift4_5,
-                s2025_gift5_1,
-                s2025_gift5_2,
-                s2025_gift5_3,
-                s2025_gift6_1,
-                s2025_gift6_2,
-                s2025_gift6_3,
-                s2025_gift7_1,
-                s2025_gift7_2
+                womanDay25Woman1_1,
+                womanDay25Woman1_2,
+                womanDay25Woman1_3,
+                womanDay25Woman1_4,
+                womanDay25Woman1_5,
+                womanDay25Woman2_1,
+                womanDay25Woman2_2,
+                womanDay25Woman2_3,
+                womanDay25Woman3_1,
+                womanDay25Woman3_2,
+                womanDay25Woman3_3,
+                womanDay25Woman3_4,
+                womanDay25Woman3_5,
+                womanDay25Woman4_1,
+                womanDay25Woman4_2,
+                womanDay25Woman4_3,
+                womanDay25Woman4_4,
+                womanDay25Woman4_5
             ], {
                 duration: 0.3,
-                // delay: '-0.3',
+                delay: '-0.5',
                 autoAlpha: 0,
                 y: '-15%',
                 stagger: 0.1,
                 // easy: 'bounce'
             })
-            .from([s2025_back], {
+            .from([womanDay25Slogan_1, womanDay25Slogan_2, womanDay25Slogan_3, womanDay25Slogan_4], {
                 duration: 1,
-                delay: '-0.7',
-                autoAlpha: 0
-            })
-            .from([s2025_made, s2025_dream], {
-                duration: 1,
-                delay: '-0.4',
+                delay: '-1',
                 autoAlpha: 0,
                 y: '-30%',
                 stagger: 0.3,
@@ -210,12 +199,14 @@ class Event_08_03_25 {
         let tl = new gsap.timeline({
             delay: delay,
             onComplete: () => {
-                this.bodyBlock.removeChild(this.anniversaryDay);
+                setTimeout(() => {
+                    this.bodyBlock.removeChild(this.womanDay25);
+                }, 1000)
             }
         });
 
         tl
-            .to(this.anniversaryDay, {
+            .to(this.womanDay25, {
                 autoAlpha: 0,
                 duration: 0.6,
                 delay: '-0.8',
