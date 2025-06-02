1334b4b1ad1f14e5888b8e3756e0da4ad243c4c8
Redvus <a.suvorof@gmail.com>
2025-06-02 13:26:20 +0400

    Добавил случайное фото.

--- src/js/RandomPlaceImage.js
+++ src/js/RandomPlaceImage.js
@@ -6,7 +6,7 @@ class RandomPlaceImage {
 		placeImage,
 		placeImageName,
 		placeLink) {
-		this.placeImage = placeImage;
+		this.placeImage = placeImage || 'https://picsum.photos/200/300/';
 		this.placeImageName = placeImageName || 'Random Place Image';
 		this.placeLink = placeLink;
 		this.initLayout();
