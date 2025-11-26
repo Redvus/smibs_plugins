878ef168d87bab414017bdfcd066f532dd87077f
Redvus <a.suvorof@gmail.com>
2025-11-26 10:06:50 +0400

    Others.

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,24 @@
-import '/scss/main.scss';
+import "/scss/app.scss";
 // import '/main.css';
 // import { } from "./";
 
 class Plugins {
     constructor() {
-        // this.initLayout();
-
+        this.initLayout();
     }
 
     initLayout() {
         this.body = document.body;
-        this.wrapper = document.querySelector('.wrapper');
+        this.wrapper = document.querySelector(".wrapper");
 
-        this.sidebar = document.createElement('div');
-        this.sidebar.className = 'sidebar';
+        this.sidebar = document.createElement("div");
+        this.sidebar.className = "sidebar";
 
-        this.sidebarStatic = document.createElement('div');
-        this.sidebarStatic.className = 'sidebar-static';
+        this.sidebarStatic = document.createElement("div");
+        this.sidebarStatic.className = "sidebar-static";
 
-        this.header = document.createElement('div');
-        this.header.className = 'header header--desktop';
+        this.header = document.createElement("div");
+        this.header.className = "header header--desktop";
         this.header.innerHTML = `
             <div class="header__nav">
                 <div class="header__top"></div>
@@ -31,11 +30,11 @@ class Plugins {
             </a>
         `;
 
-        this.mainContent = document.createElement('div');
-        this.mainContent.className = 'main-content';
+        this.mainContent = document.createElement("div");
+        this.mainContent.className = "main-content";
 
-        this.mainContentInside = document.createElement('div');
-        this.mainContentInside.className = 'main-content__inside';
+        this.mainContentInside = document.createElement("div");
+        this.mainContentInside.className = "main-content__inside";
 
         // this.footer = document.createElement('div');
         // this.footer.className = 'footer';
@@ -51,4 +50,4 @@ class Plugins {
     }
 }
 
-export { Plugins }
+export { Plugins };
