0920078694a1cfe35d21ae7a31148920c61d3d1c
Redvus <a.suvorof@gmail.com>
2025-12-11 13:13:02 +0400

    Удалил лишнее.

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 // import { InjectCssToJsPlugin } from 'vite-plugin-inject-css-to-js';
 
 export default defineConfig({
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-        {
-            host: true, // Open to local network and display URL
-            // open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
-        },
+    root: "src/",
+    publicDir: "../static/",
+    base: "./",
+    server: {
+        host: true, // Open to local network and display URL
+        // open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
     build: {
         // sourcemap: true, // Add sourcemap
-        outDir: '../dist/', // Output in the dist/ folder
+        outDir: "../dist/", // Output in the dist/ folder
         // emptyOutDir: true, // Empty the folder first
         // plugins: [
         //     InjectCssToJsPlugin({
@@ -27,16 +26,16 @@ export default defineConfig({
             output: {
                 // chunkFileNames: 'assets/js/[name]-[hash].js',
                 // entryFileNames: 'assets/js/[name]-[hash].js',
-                chunkFileNames: 'js/app-min.js',
-                entryFileNames: 'js/app-min.js',
+                chunkFileNames: "app-min.js",
+                entryFileNames: "app-min.js",
 
-                assetFileNames: ({names}) => {
-                    if (/\.(gif|jpg|jpe?g|png|svg)$/.test(names ?? '')){
-                        return 'images/[name][extname]';
+                assetFileNames: ({ names }) => {
+                    if (/\.(gif|jpg|jpe?g|png|svg)$/.test(names ?? "")) {
+                        return "images/[name][extname]";
                     }
 
-                    if (/\.css$/.test(names ?? '')) {
-                        return 'css/app.css';
+                    if (/\.css$/.test(names ?? "")) {
+                        return "app.css";
                     }
 
                     // if (/\.(woff|woff2)$/.test(name ?? '')) {
@@ -45,13 +44,13 @@ export default defineConfig({
 
                     // default value
                     // ref: https://rollupjs.org/guide/en/#outputassetfilenames
-                    return '[name]-[hash][extname]';
+                    return "[name]-[hash][extname]";
                 },
             },
-        }
+        },
     },
     css: {
-        devSourcemap: true
+        devSourcemap: true,
         // sourcemap: true
         // preprocessorOptions: {
         //     scss: {
@@ -62,5 +61,5 @@ export default defineConfig({
         //         `
         //     }
         // }
-    }
+    },
 });
