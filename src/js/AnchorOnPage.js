79910b45d502f0506923335cff2a059dd39df0b5
Redvus <a.suvorof@gmail.com>
2025-12-18 08:49:28 +0400

    Убрал один якорь.

--- src/js/AnchorOnPage.js
+++ src/js/AnchorOnPage.js
@@ -37,17 +37,16 @@ class AnchorOnPage {
             this.anchorDay_12,
             this.anchorDay_13,
             this.anchorDay_14,
-            this.anchorDay_15,
-            this.anchorDay_16
+            this.anchorDay_15
         ];
 
-        this.anchorDayNum = ['17 / 12', '18 / 12', '21 / 12', '22 / 12', '23 / 12', '24 / 12', '25 / 12', '26 / 12', '28 / 12', '29 / 12', '3 / 01', '4 / 01', '5 / 01', '6 / 01', '8 / 01', '9 / 01', '11 / 01'];
+        this.anchorDayNum = ['17 / 12', '21 / 12', '22 / 12', '23 / 12', '24 / 12', '25 / 12', '26 / 12', '28 / 12', '29 / 12', '3 / 01', '4 / 01', '5 / 01', '6 / 01', '8 / 01', '9 / 01', '11 / 01'];
     }
 
     initAnchorBlock() {
         this.anchorDayBlock = document.createElement('ul');
         this.anchorDayBlock.className = 'anchor-day-block';
-        this.anchorDayCount = 17;
+        this.anchorDayCount = 16;
 
         for (let i = 0; i < this.anchorDayCount; i++) {
             const anchorDayLink = document.createElement('li');
