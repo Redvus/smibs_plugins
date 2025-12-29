308474a4e1ca2c2adebdc0d2d268e8b7ef223100
Redvus <a.suvorof@gmail.com>
2025-12-29 12:05:45 +0400

    Изменил конфиг снеговика.

--- src/js/utils/config.js
+++ src/js/utils/config.js
@@ -2,16 +2,16 @@ export const GAME_CONFIG = {
     TOTAL_PAGES: 10,
 
     SNOWMAN_PARTS: [
-        { id: 'bottom', name: 'Нижний шар', emoji: '⛄', order: 1 },
-        { id: 'middle', name: 'Средний шар', emoji: '⛄', order: 2 },
-        { id: 'top', name: 'Верхний шар', emoji: '⛄', order: 3 },
-        { id: 'hat', name: 'Шляпа', emoji: '🎩', order: 4 },
-        { id: 'scarf', name: 'Шарф', emoji: '🧣', order: 5 },
-        { id: 'leftArm', name: 'Левая рука', emoji: '↙️', order: 6 },
-        { id: 'rightArm', name: 'Правая рука', emoji: '↘️', order: 7 },
-        { id: 'leftEye', name: 'Левый глаз', emoji: '⚫', order: 8 },
-        { id: 'rightEye', name: 'Правый глаз', emoji: '⚫', order: 9 },
-        { id: 'nose', name: 'Нос', emoji: '🥕', order: 10 }
+        { id: 'part-1', name: 'BottomCircle', image: 'snowman_1Circle.webp', order: 1 },
+        { id: 'part-2', name: 'MiddleCircle', image: 'snowman_2Circle.webp', order: 2 },
+        { id: 'part-3', name: 'TopCircle', image: 'snowman_3Circle.webp', order: 3 },
+        { id: 'part-4', name: 'Bucket', image: 'snowman_bucket.webp', order: 4 },
+        { id: 'part-5', name: 'Scarf', image: 'snowman_scurf.webp', order: 5 },
+        { id: 'part-6', name: 'Hands', image: 'snowman_hands.webp', order: 6 },
+        { id: 'part-7', name: 'Mittens', image: 'snowman_mittens.webp', order: 7 },
+        { id: 'part-8', name: 'Broomstick', image: 'snowman_broomstick.webp', order: 8 },
+        { id: 'part-9', name: 'Face', image: 'snowman_face.webp', order: 9 },
+        { id: 'part-10', name: 'Nose', image: 'snowman_nose.webp', order: 10 }
     ],
 
     QUESTIONS: {
