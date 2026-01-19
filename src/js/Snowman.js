ee2a4443cd314a5ced54f1dab671cae02c12517c
Redvus <a.suvorof@gmail.com>
2026-01-19 14:48:47 +0400

    Убрал поздравление для второй игры.

--- src/js/Snowman.js
+++ src/js/Snowman.js
@@ -1,105 +1,106 @@
 import { gsap } from "gsap";
-import { GAME_CONFIG } from './utils/config.js'
-import { Snowfall } from './components/Snowfall.js';
+import { GAME_CONFIG } from "./utils/config.js";
+import { Snowfall } from "./components/Snowfall.js";
 
 class SnowmanGame {
     constructor() {
         this.initSnowman();
 
-        const snowmanLoad = document.getElementById('snowmanLoad');
-        if (snowmanLoad) {
-            snowmanLoad.addEventListener('click', () => {
-                this.initAnimation();
-            });
-        }
+        const snowmanLoad = document.getElementById("snowmanLoad");
+        // if (snowmanLoad) {
+        //     snowmanLoad.addEventListener("click", () => {
+        this.initAnimation();
+        //     });
+        // }
 
         // this.initDev();
     }
 
     initSnowman() {
         this.bodyBlock = document.body;
-        this.snowmanContainer = document.createElement('div');
-        this.snowmanContainer.id = 'snowman2026';
-        this.snowmanContainer.classList = 'snowman';
+        this.snowmanContainer = document.createElement("div");
+        this.snowmanContainer.id = "snowman2026";
+        this.snowmanContainer.classList = "snowman";
 
-        this.snowmanSnowdrift = document.createElement('div');
-        this.snowmanSnowdrift.id = 'snowmanSnowdrift';
+        this.snowmanSnowdrift = document.createElement("div");
+        this.snowmanSnowdrift.id = "snowmanSnowdrift";
         this.snowmanSnowdrift.innerHTML = `
             <picture>
                 <img src="/assets/games/snowman/images/snowman_snowdrift.webp">
             </picture>
         `;
 
-        let html = '<div class="snowman__block" id="snowman">'
-        GAME_CONFIG.SNOWMAN_PARTS.forEach(part => {
+        let html = '<div class="snowman__block" id="snowman">';
+        GAME_CONFIG.SNOWMAN_PARTS.forEach((part) => {
             // const hasPart = this.gameState.collectedParts.includes(part.id)
             html += `
                 <picture class="snowman__part" id="${part.id}">
                     <img src="/assets/games/snowman/images/${part.image}">
                 </picture>
-            `
-        })
-        html += '</div>'
+            `;
+        });
+        html += "</div>";
         this.snowmanContainer.innerHTML = html;
 
-        this.snowmanTitle = document.createElement('div');
-        this.snowmanTitle.classList = 'snowman__titles';
+        this.snowmanTitle = document.createElement("div");
+        this.snowmanTitle.classList = "snowman__titles";
 
-        this.snowmanTitle_1 = document.createElement('div');
-        this.snowmanTitle_2 = document.createElement('div');
+        this.snowmanTitle_1 = document.createElement("div");
+        this.snowmanTitle_2 = document.createElement("div");
 
-        let title_1 = '<div class="snowman__title snowman__title--1" id="snowmanTitle1">'
-        GAME_CONFIG.SNOWMAN_TITLE_1.forEach(part => {
+        let title_1 =
+            '<div class="snowman__title snowman__title--1" id="snowmanTitle1">';
+        GAME_CONFIG.SNOWMAN_TITLE_1.forEach((part) => {
             title_1 += `
                 <picture class="snowman__title_part" id="${part.id}">
                     <img src="/assets/games/snowman/images/titles/${part.image}.png">
                 </picture>
-            `
-        })
-        title_1 += '</div>'
+            `;
+        });
+        title_1 += "</div>";
 
-        let title_2 = '<div class="snowman__title snowman__title--1" id="snowmanTitle1">'
-        GAME_CONFIG.SNOWMAN_TITLE_2.forEach(part => {
+        let title_2 =
+            '<div class="snowman__title snowman__title--1" id="snowmanTitle1">';
+        GAME_CONFIG.SNOWMAN_TITLE_2.forEach((part) => {
             title_2 += `
                 <picture class="snowman__title_part" id="${part.id}">
                     <img src="/assets/games/snowman/images/titles/${part.image}.png">
                 </picture>
-            `
-        })
-        title_2 += '</div>'
+            `;
+        });
+        title_2 += "</div>";
 
         this.bodyBlock.appendChild(this.snowmanContainer);
         this.snowmanContainer.appendChild(this.snowmanSnowdrift);
         this.snowmanContainer.appendChild(this.snowmanTitle);
-        this.snowmanTitle.innerHTML = ( title_1 + title_2 );
+        // this.snowmanTitle.innerHTML = title_1 + title_2;
+        this.snowmanTitle.innerHTML = title_1;
     }
 
     initAnimation() {
-        const
-            snowman2026 = document.getElementById("snowman2026"),
+        const snowman2026 = document.getElementById("snowman2026"),
             snowmanParts = [...snowman2026.querySelectorAll(".snowman__part")],
-            snowmanTitleParts = [...snowman2026.querySelectorAll(".snowman__title_part")]
-        ;
-
+            snowmanTitleParts = [
+                ...snowman2026.querySelectorAll(".snowman__title_part"),
+            ];
         let tl = new gsap.timeline({
-            delay: 0,
+            delay: 1,
             onStart: () => {
                 // this.bodyBlock.style.overflow = "hidden";
                 // setTimeout(() => {
                 //     new Snowfall();
                 // }, 700);
             },
-            onComplete: this.initHide(7),
+            onComplete: this.initHide(8),
         });
 
-        tl
-            .to(snowman2026, {
-                duration: 0.3,
-                delay: "0.3",
-                autoAlpha: 1,
-                zIndex: 9999,
-                // easy: "elastic.in(1,0.3)"
-            })
+        tl.to(snowman2026, {
+            duration: 0.3,
+            delay: "0.3",
+            autoAlpha: 1,
+            zIndex: 9999,
+            // easy: "elastic.in(1,0.3)"
+        })
             .from(snowmanParts, {
                 duration: 1,
                 delay: "0.1",
@@ -115,8 +116,7 @@ class SnowmanGame {
                 stagger: 0.2,
                 y: "-=10",
                 ease: "elastic.out(1, 0.5)",
-            })
-        ;
+            });
     }
 
     initDev() {
@@ -124,11 +124,10 @@ class SnowmanGame {
         this.snowmanContainer.style.visibility = "visible";
         this.snowmanContainer.style.zIndex = 9999;
         this.bodyBlock.style.overflow = "hidden";
-
     }
 
     initHide(delay) {
-        const snowfallCanvas = document.getElementById('snowfallCanvas');
+        const snowfallCanvas = document.getElementById("snowfallCanvas");
 
         let tl = new gsap.timeline({
             delay: delay,
